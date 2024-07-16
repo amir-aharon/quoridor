101db23aa70147b3659080f1c3a596dba3a1665c
Amir <amir.the.junior@gmail.com>
2024-07-16 21:02:15 +0300

    Setup env.

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(
+    name="quoridor",
+    packages=find_packages("."),
+    version="1.0.0",
+    install_requires=[],
+)
