28083f0d0cfe4ed8b890b11929ea6bb0192f525b
Amir Aharon <51268843+amir-aharon@users.noreply.github.com>
2024-07-20 11:52:21 +0300

    Finished main game's board graphic. (#2)
    * Added board class.
    
    * In progress
    
    * Finished starting point for board and player.
    
    * Finished game display.

--- src/quoridor/player.py
+++ src/quoridor/player.py
@@ -1,13 +1,14 @@
 from typing import Tuple
 
-from .borad import Board, Direction
+from .board import Board, Direction
 
 
 class Player:
-    def __init__(self, pos: Tuple[int, int]):
-        self.pos = pos
+    def __init__(self, position: Tuple[int, int]):
+        self.position = position
         self.walls_left = 10
 
-    def request_place_wall(self, board: Board, pos: Tuple[int, int], direction: Direction) -> None:
+    def request_place_wall(self, board: Board, position: Tuple[int, int], direction: Direction) -> None:
         if self.walls_left > 0:
-            return board.place_wall(self, pos, direction)
+            return board.place_wall(self, position, direction)
+          
